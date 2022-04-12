728e89d910edc66f3409c3215ca6dd6eb654808c
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-04-11 21:48:36 -0600

    fixes has_many associations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,9 @@
 class Meeting < ApplicationRecord
   has_many :speakers
-  has_many :speakers, through: :speaker_meetings
+  has_many :speakers, through: :meeting_speakers
+
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :location, presence: true
+  validates :time, presence: true
 end
